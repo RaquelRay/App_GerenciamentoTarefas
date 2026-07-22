b4f126f0ce896ef929eab3a4af95c5e1443aec59
Raquel <raquelazevedo.produtora@gmail.com>
2026-07-21 21:34:53 -0300

    Recriando a pasta do projeto

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,34 @@
+import {
+  ChevronRightIcon,
+  DeleteIcon,
+  Trash2Icon,
+  TrashIcon,
+} from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-left text-white p-2 rounded=md w-full ${task.isCompleted && "line-through"}`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.onDeleteTaskClick(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <Trash2Icon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
