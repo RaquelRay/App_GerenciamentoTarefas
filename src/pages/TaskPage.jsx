654ea0231bd3417e4152845fab547e47300336b1
Raquel <raquelazevedo.produtora@gmail.com>
2026-07-23 17:36:08 -0300

    estilizei a page de detalhes, e acrescentei botao de voltar

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,14 +1,32 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
 
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <button
+            onClick={() => navigate(-1)}
+            className="text-slate-100 absolute left-0 top-0 bottom-0"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3x1 text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl text-slate-600 font-bold">{title}</h2>
+          <p className="text-slate-400">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
