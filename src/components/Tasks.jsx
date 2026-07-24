88ad681e835bb8b4797b46c3a4d8e47f807aa49a
Raquel <raquelazevedo.produtora@gmail.com>
2026-07-24 17:14:33 -0300

    colocando icone de taskCompleted

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, Trash2Icon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -18,8 +18,9 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left text-white p-2 rounded=md w-full ${task.isCompleted && "line-through"}`}
+            className={`bg-slate-400 text-left text-white p-2 rounded=md w-full flex items-center gap-2 ${task.isCompleted && "line-through"}`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
